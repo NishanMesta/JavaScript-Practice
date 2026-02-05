1fba7033f439043c57aa342f6aa7d936bd12ae34
NishanMesta <nishanmesta24@gmail.com>
2026-02-06 00:43:02 +0530

    Feb5

---
+++ Arrays.js
@@ -0,0 +1,28 @@
+function islegal(user){
+    if (user.age<18){
+        console.log(user.name+ " is Not eligible to vote")
+        }else{
+        console.log(user.name+ " is eligible to vote")
+        }
+}
+var user1=[{
+            name:"Vishwa",
+            age:19,
+            address:{
+                        city:"Banglore",
+                        District:"banglore North"
+                    }
+          },{
+             name:"Venkatesh",
+                         age:39,
+                         address:{
+                                     city:"Banglore",
+                                     District:"banglore North"
+                         }
+          }];
+for (var i=0;i<2;i++){
+    islegal(user1[i])
+ }
+
+ console.log(user1[0])
+
