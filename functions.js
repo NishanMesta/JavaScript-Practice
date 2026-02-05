2776ed73246aa41f1e29097e54e0bdd1e8fc4c04
NishanMesta <nishanmesta24@gmail.com>
2026-02-05 23:03:31 +0530

    first commit

---
+++ functions.js
@@ -0,0 +1,12 @@
+function greet(Sayhello){
+console.log("hello "+Sayhello);
+console.log("Namaste "+Sayhello);
+console.log("Salaam "+Sayhello);
+}
+let firstname="Simha";
+let firstname1= "shiva";
+let firstname2="java";
+
+greet(firstname)
+greet(firstname1)
+greet(firstname2)
---
+++ ifloop.js
@@ -0,0 +1,10 @@
+function islegal(age){
+    if (age>=18){
+    console.log("Eligible to vote")}
+    else{
+    console.log("Not Eligible to vote")}
+}
+
+for (let i=0;i<=49;i++){
+    islegal(i)
+    }
